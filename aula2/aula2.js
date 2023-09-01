85c4f89e21621684d7ca4fd4ed865b8bb64f77cf
mistermarques <gabrielmarques.sp@hotmail.com>
2023-08-31 22:22:16 -0300

    novo

--- aula2/aula2.js
+++ aula2/aula2.js
@@ -1,5 +1,25 @@
+/*
 let nome = "cassio";
 
 console.log(nome);
 
-alert("Gabriel");
+//alert("Gabriel");
+
+//nome = prompt("digite seu nome aqui");
+
+//alert(`ola boas noite  ${nome}`);
+*/
+
+//let number1 = Number (prompt("digita um numero aqui"));
+//let number2 = Number (prompt("digita outro numero ai"));
+
+//alert(number1 + number2);
+
+let number1 = parseFloat(prompt("digite um numero aqui"));
+let number2 = parseFloat(prompt("digite outro aqui "));
+
+//let result = number1 + number2;
+
+//alert(result);
+
+alert(number1 - number2);
